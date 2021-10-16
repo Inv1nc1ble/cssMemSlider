6607b6e94baac46ead15f1b392cc324d2f32c22f
Stas <stasprokopyshyn@gmail.com>
2021-10-16 16:59:32 +0300

    feat: started js (Sat, Oct 16, 2021, 17:00)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("DOM fully loaded");
+    const wrapper = document.querySelector('.slider-wrapper');
+    const sliderInner = document.querySelector('.slider-inner');
+    const slides = document.querySelectorAll('.slider-item');
+    
+
+    const phrases = [
+        'Be cute as a Duck',
+        'Be bright as a Duck',
+        'Fly as a Duck',
+        'Have a Family as a Duck',
+        'Be beautiful as a Duck',
+    ];
+
+
+    const wrapperWidth = window.getComputedStyle(wrapper).width;
+    let slideIndex = 1;
+    let offset = 0;
+
+    sliderInner.style.width = 100 * slides.length + '%';
+
+    slides.forEach(slide => {
+        slide.style.width = wrapperWidth;
+    });
+
+    sliderInner.style.transform = `translateX(-0px)`;
+
+
+
+
 });
