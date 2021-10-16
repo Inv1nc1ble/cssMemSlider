39d2437ef8bde2cfd74614128369d7ca9f6fe13e
Stas <stasprokopyshyn@gmail.com>
2021-10-16 18:11:45 +0300

    feat: animation added (Sat, Oct 16, 2021, 18:11)

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
     const wrapper = document.querySelector('.slider-wrapper');
+    const title = document.querySelector('.slider-title');
     const sliderInner = document.querySelector('.slider-inner');
     const slides = document.querySelectorAll('.slider-item');
+    const controls = document.querySelectorAll('.control-item');
+    const dots = document.querySelectorAll('.control-item-dot');
     
 
     const phrases = [
         'Be cute as a Duck',
-        'Be bright as a Duck',
+        'Quack as a Duck',
         'Fly as a Duck',
-        'Have a Family as a Duck',
+        `Don't mess with a Duck` ,
         'Be beautiful as a Duck',
     ];
-
+    title.textContent = phrases[0];
 
     const wrapperWidth = window.getComputedStyle(wrapper).width;
     let slideIndex = 1;
@@ -23,9 +26,39 @@ document.addEventListener("DOMContentLoaded", () => {
         slide.style.width = wrapperWidth;
     });
 
-    sliderInner.style.transform = `translateX(-0px)`;
+    function removeActiveDots() {
+        dots.forEach(dot => {
+            if (dot.classList.contains('active')) {
+                dot.classList.remove('active')
+            }
+        });
+    }
 
+    function setTitle(i) {
+        title.classList.add('hide');
+        setTimeout(function() {
+            title.textContent = phrases[i];
+        }, 500);
+        setTimeout(function() {
+            title.classList.remove('hide');
+        }, 500);
+        
+        
+    }
 
+    controls.forEach((control, i) => {
+        control.addEventListener('click', () => {
+            let w = +wrapperWidth.slice(0,wrapperWidth.length-2);
+            offset = i * w;
+            sliderInner.style.transform = `translateX(-${offset}px)`;
+
+            if (!dots[i].classList.contains('active')) {
+                removeActiveDots();
+                setTitle(i);
+                dots[i].classList.add('active');
+            }
+        })
+    })
 
 
 });
